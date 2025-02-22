769abcee73eff6d006649b9655789b9cb1b03f2a
Arianna Masciolini <uzkamascio@gmail.com>
2025-02-22 13:50:15 +0100

    update csv automatically

--- pockebooks.py
+++ pockebooks.py
@@ -7,17 +7,18 @@ import sys
 if __name__ == "__main__":
     with open(sys.argv[1]) as infile:
         csv_reader = csv.DictReader(infile)
-        unarts = [(row["title"],row["url"]) for row in csv_reader if row["status"] == "unread" and not row["tags"]]
-    domains = list(set([urlparse(url).netloc for (_,url) in unarts]))
+        unarts = [(i,row["title"],row["url"]) for (i,row) in enumerate(csv_reader) if row["status"] == "unread" and not row["tags"]]
+    domains = list(set([urlparse(url).netloc for (_,_,url) in unarts]))
 
     for domain in sorted(domains):
-        domain_unarts = [unart for unart in unarts if urlparse(unart[1]).netloc == domain]
+        domain_unarts = [unart for unart in unarts if urlparse(unart[2]).netloc == domain]
         filename = domain + ".epub"
 
         if not exists(filename):
+            print()
             print(domain)
             print()
-            for (n, (title,url)) in enumerate(domain_unarts):
+            for (n, (i,title,url)) in enumerate(domain_unarts):
                 print(str(n + 1) + ".", title, "({})".format(url))
 
             title = input("title: ")
@@ -30,5 +31,14 @@ if __name__ == "__main__":
                 if i + 1 not in excludes:
                     selection.append(unart)
 
-            if title != "" or author != "":
-                subprocess.run(["percollate", "epub"] + [url for (_,url) in selection] + ["-o", filename, "-t", title, "-a", author]) 
+            if title != "" or author != "": 
+                # create ebook
+                subprocess.run(["percollate", "epub"] + [url for (_,_,url) in selection] + ["-o", filename, "-t", title, "-a", author]) 
+                # update CSV (set selection to "reading")
+                with open(sys.argv[1], "r") as infile:
+                    rows = infile.readlines()
+                for (i,_,_) in selection:
+                    rows[i + 1] = rows[i + 1].replace(",unread", ",reading")
+                with open(sys.argv[1], 'w') as infile:
+                    infile.writelines(rows)
+            
